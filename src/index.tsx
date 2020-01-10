dbc66135047e3777f106937439a8d17e1ab82055
trungnx <trung@kyber.network>
2020-01-10 15:55:31 +0700

    Add code splitting based on route

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware, Middleware } from 'redux';
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { logger } from 'redux-logger';
 import 'src/assets/styles/index.scss';
-import App from 'src/+app/components/App';
 import GlobalErrorBoundary from 'src/+app/components/error-boundaries/GlobalErrorBoundary';
 import * as serviceWorker from './serviceWorker';
 import rootSaga from 'src/+app/sagas';
@@ -14,26 +13,27 @@ import rootReducer from 'src/+app/reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 let middleware: Middleware[] = [sagaMiddleware];
-
-if (process.env.NODE_ENV === 'development') {
-  middleware = [...middleware, logger];
-}
-
+if (process.env.NODE_ENV === 'development') middleware = [...middleware, logger];
 const store = createStore(rootReducer, applyMiddleware(...middleware));
-
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render((
+const AppComponent = lazy(() => import('src/+app/components/App'));
+
+const App = (
   <GlobalErrorBoundary>
     <Provider store={store}>
       <Router>
-        <Switch>
-          <Route exact path="/customPath" component={App} />
-          <Redirect to={`/customPath`}/>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/customPath" component={AppComponent} />
+            <Redirect to={`/customPath`}/>
+          </Switch>
+        </Suspense>
       </Router>
     </Provider>
   </GlobalErrorBoundary>
-), document.getElementById('root'));
+);
+
+ReactDOM.render(App, document.getElementById('root'));
 
 serviceWorker.unregister();
